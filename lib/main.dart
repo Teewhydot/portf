8af1d4a019508aa366dc86a0b72cc25684790e8c
Tunde <administrator@teeofgui.tech>
2023-05-18 16:06:40 +0100

    Width adjustment

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class Portfolio extends StatelessWidget {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
       home: LayoutBuilder(builder: (BuildContext context,BoxConstraints boxConstraints){
-        if(boxConstraints.maxWidth < 600){
+        if(boxConstraints.maxWidth < 550){
           return const CustomHomeTranslate();
         }else {
           return const Scaffold(
